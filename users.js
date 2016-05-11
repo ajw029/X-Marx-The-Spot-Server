3d377f279782dbb1326b25ff41ff833933fef8d0
Anthony Wang <ajw029@ucsd.edu>
2016-05-11 00:54:40 -0700

    Updated sign in to create default folder

--- users.js
+++ users.js
@@ -5,6 +5,7 @@ var db = require('./db');
 
 var user_table = tables.user_table;
 var account_table = tables.user_table;
+var folder_table = tables.folder_table;
 
 /**
  *
@@ -70,8 +71,9 @@ module.exports.loginAuth = function(req, response) {
           response.redirect('/login');
         }
         else {
+
           var queryP = res[0].password;
-          if(password.toString()==queryP.toString()){
+          if(encryption.encrypt(password.toString())==queryP.toString()){
             req.session.username = encryption.encrypt(res[0].id.toString());
             response.redirect('/bookmarx');
           }
@@ -97,32 +99,41 @@ module.exports.signupAuth = function(req, response) {
   var password = req.body.pass;
   var repassword = req.body.repass;
 
-  if(password==repassword){
+  if(password==repassword && password.trim()){
 
      var queryString="SELECT username from "+ user_table +" WHERE username="+ "\"" + username+ "\"";
 
       db.query(queryString,function(err,res){
 
-      if(err){
+      // Redirect back to Sign Up if Account Exists
+      if(err || res[0]){
         response.redirect('/signup');
-      }if(res){
-        // Redirect back to Sign Up if Account Exists
-        if(res[0]){
-          response.redirect('/signup');
-        }else {
-          // not exists create new one
-          var createUserQueryString="INSERT INTO "+ user_table+" (username,password) values ("+ "\"" +username+"\"" +","+"\"" +password+"\"" +");"
-          db.query(createUserQueryString,function(err,res){
-            if(err){
-            }else if(res){
-              var getAccountId="SELECT * from "+ user_table +" WHERE username="+ "\"" + username + "\"";
-              db.query(getAccountId,function(err, accountId){
-                req.session.username = encryption.encrypt(accountId[0].id.toString());
-                response.redirect('/login');
+      }if(res && !res[0]){
+        // not exists create new one
+
+        // Encrypt Pwd
+        var encryptedPwd = encryption.encrypt(password);
+        var createUserQueryString="INSERT INTO "+ user_table+" (username,password) values ("+ "\"" +username+"\"" +","+"\"" +encryptedPwd+"\"" +");"
+        db.query(createUserQueryString,function(err,res){
+          if(err){
+          }else if(res){
+            var getAccountId="SELECT * from "+ user_table +" WHERE username="+ "\"" + username + "\"";
+            db.query(getAccountId,function(err, accountId){
+              var createDefaultFolder = "INSERT INTO " + folder_table + " (account_id, name) VALUES(" +db.escape(accountId[0].id.toString()) +", 'Default' )";
+              db.query(createDefaultFolder,function(err, folderRes){
+                if (err) {
+                  throw err;
+                }
+                if (folderRes) {
+                  req.session.username = encryption.encrypt(accountId[0].id.toString());
+                  response.redirect('/login');
+                }
               });
-            }
-          });
-        }
+
+            });
+
+          }
+        });
 
       }
    });
@@ -135,11 +146,10 @@ module.exports.signupAuth = function(req, response) {
 
 module.exports.updatepassword=function(req,res){
   var account_id = db.escape(req.body.account_id);
-  var oldPassword=db.escape(req.body.oldPassword);
-  var newPassword=db.escape(req.body.newPassword);
-  var reNewPassword=db.escape(req.body.reNewPassword);
 
-  console.log(account_id+oldPassword+newPassword+reNewPassword);
+  var oldPassword=req.body.oldPassword;
+  var newPassword=req.body.newPassword;
+  var reNewPassword=req.body.reNewPassword;
 
   if(oldPassword&&newPassword&&reNewPassword){
     if(newPassword==reNewPassword){
@@ -147,24 +157,30 @@ module.exports.updatepassword=function(req,res){
         db.query(queryPasswordString,function(err,res1){
           if(err){
             throw err;
-            res.redirect('/settings');
+            res.redirect('/bookmarx/settings');
           }
-          if(res1){
-            var updatePasswordQuery="UPDATE "+ user_table+" set password=" + newPassword+ " where id="+account_id;
+
+          if(res1[0] && res1[0].password==encryption.encrypt(oldPassword)){
+
+            var updatePasswordQuery="UPDATE "+ user_table+" set password=" + db.escape(encryption.encrypt(newPassword))+ " where id="+account_id;
             db.query(updatePasswordQuery,function(err,res2){
               if(err){
                 throw err;
-                res.redirect('/settings');
+                res.redirect('/bookmarx/settings');
               }
               if(res2){
                 res.redirect('/bookmarx');
               }
             });
           }
+          else {
+            res.redirect('/bookmarx/settings');
+          }
+
         });
 
     }else{
-      res.redirect('/settings');
+      res.redirect('/bookmarx/settings');
     }
   }
 
