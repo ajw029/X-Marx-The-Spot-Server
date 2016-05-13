f52c9b827befefd3f011ab671fd1a0691ff0ac84
Leo Wong <LeoWW@users.noreply.github.com>
2016-05-12 23:46:34 -0700

    original my_configs.js

---
+++ my_configs.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ECRYPT_KEY: "aes-256-ctr",
+  ENCRYPT_PASS: "OMG_SUCH_DOGE_666_777",
+  SESSION_KEY: 'OMG_NODE_SESSIONc00l',
+  DATABASE_HOST: 'localhost',
+  DATABASE_USER: 'root',
+  DATABASE_PASSWORD: '12345678',
+  DATABASE_PORT: 3306,
+  DATABASE_NAME: 'bookmarx',
+  PORT: 5001
+};
